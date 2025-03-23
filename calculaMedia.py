4fe5f37afe5c6450f9ba26410354cb7b22db3787
leonardo-c3p <leonardo.ufms@gmail.com>
2025-03-22 22:21:24 -0400

    Add files via upload
    Programa em python para cálculo da média aritmética de dois números inteiros positivos

---
+++ calculaMedia.py
@@ -0,0 +1,12 @@
+#programa calculaMedia
+
+#Leitura dos valores
+print("Digite dois numeros\n")
+num1 = float(input("Numero X: "))
+num2 = float(input("Numero Y: "))
+
+#Calculo da média aritmética
+resposta = (num1 + num2)/2
+
+#Resultado
+print("Media de X e Y é igual a ", resposta)
